48d30a1d640f9601bba2a8f29f2ef798f9528cdd
Chris <c.trentham87@gmail.com>
2025-04-18 00:06:38 -0500

    created function to copy files from the static folder to the public folder.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 from textnode import *
+import os
+import shutil
+
+def copy_recursive(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.makedirs(dst)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copy_recursive(s, d)
+        else:
+            shutil.copy2(s, d)
+            print(f"Copied: {d}")
 
 def main():
-    test_node = TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
-    print(test_node)
+    source_dir = "static"
+    dest_dir = "public"
+    copy_recursive(source_dir, dest_dir)
 
 main()
